d6ae779748eb3a2a6292993aeb119c55e2d6a03f
Maryhasalittlesheep <78140403+Maryhasalittlesheep@users.noreply.github.com>
2021-04-16 04:09:44 +0800

    some updated

--- sketch.js
+++ sketch.js
@@ -25,66 +25,78 @@ var playerAnimation;
 
 // Allocate Adventure Manager with states table and interaction tables
 function preload() {
-  adventureManager = new AdventureManager("data/adventureStates.csv", "data/interactionTable.csv");
+    adventureManager = new AdventureManager("data/adventureStates.csv", "data/interactionTable.csv");
 
 }
 
 // Setup the adventure manager
 function setup() {
-  createCanvas(1280, 720);
+    createCanvas(1280, 720);
 
-  // This will load the images, go through state and interation tables, etc
-  adventureManager.setup();
+    // This will load the images, go through state and interation tables, etc
+    adventureManager.setup();
 
     // create a sprite 
-  playerSprite = createSprite(width/2, height/2, 80, 80);
-  playerSprite.addAnimation('regular', 'assets/1.png', 'assets/3.png');
+    playerSprite = createSprite(width/2, height/2, 80, 80);
+    playerSprite.addAnimation('still','assets/1.png');
+    playerSprite.addAnimation('left', 'assets/sprite/4.png', 'assets/sprite/6.png');
+    playerSprite.addAnimation('right', 'assets/sprite/7.png', 'assets/sprite/9.png');
+    playerSprite.addAnimation('up', 'assets/sprite/10.png', 'assets/sprite/12.png');
+    playerSprite.addAnimation('down', 'assets/sprite/1.png', 'assets/sprite/3.png');
 
-  // use this to track movement from toom to room in adventureManager.draw()
-  adventureManager.setPlayerSprite(playerSprite);
+  	// use this to track movement from toom to room in adventureManager.draw()
+	adventureManager.setPlayerSprite(playerSprite);
 }
 
 // Adventure manager handles it all!
 function draw() {
-  // draws background rooms and handles movement from one to another
-  adventureManager.draw();
+    // draws background rooms and handles movement from one to another
+    adventureManager.draw();
 
-  // responds to keydowns
-  moveSprite();
+    // responds to keydowns
+    moveSprite();
 
-  // this is a function of p5.js, not of this sketch
-  drawSprites();
+    // this is a function of p5.js, not of this sketch
+    drawSprites();
 }
 
 // pass to adventure manager, this do the draw / undraw events
 function keyPressed() {
-  // toggle fullscreen mode
-  if( key === 'f') {
-    fs = fullscreen();
-    fullscreen(!fs);
-  }
+    // toggle fullscreen mode
+    if( key === 'f') {
+      fs = fullscreen();
+      fullscreen(!fs);
+    }
 
-  // dispatch key events for adventure manager to move from state to 
-  // state or do special actions - this can be disabled for NPC conversations
-  // or text entry
-  adventureManager.keyPressed(key);  
+    // dispatch key events for adventure manager to move from state to 
+    // state or do special actions - this can be disabled for NPC conversations
+    // or text entry
+    adventureManager.keyPressed(key);  
 }
 
 //-------------- YOUR SPRITE MOVEMENT CODE HERE  ---------------//
 function moveSprite() {
-  if(keyIsDown(RIGHT_ARROW))
-    playerSprite.velocity.x = 10;
-  else if(keyIsDown(LEFT_ARROW))
-    playerSprite.velocity.x = -10;
-  else
-    playerSprite.velocity.x = 0;
-
-  if(keyIsDown(DOWN_ARROW))
-    playerSprite.velocity.y = 10;
-  else if(keyIsDown(UP_ARROW))
-    playerSprite.velocity.y = -10;
-  else
-    playerSprite.velocity.y = 0;
+    if(keyIsDown(RIGHT_ARROW)){
+    	playerSprite.changeAnimation("right");
+        playerSprite.velocity.x = 8;
+    }
+    else if(keyIsDown(LEFT_ARROW)){
+    	playerSprite.changeAnimation("left");
+        playerSprite.velocity.x = -8;
+    }
+    else if(keyIsDown(DOWN_ARROW)){
+    	playerSprite.changeAnimation("down");
+  	    playerSprite.velocity.y = 8;
+  	}
+    else if(keyIsDown(UP_ARROW)){
+    	playerSprite.changeAnimation("up");
+        playerSprite.velocity.y = -8;
+    }
+ 	else{
+    	playerSprite.changeAnimation("still");
+        playerSprite.velocity.x = 0;
+        playerSprite.velocity.y = 0;
+    }
 }
 
 
